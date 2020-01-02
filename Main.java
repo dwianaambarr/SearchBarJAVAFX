8c7034ca90e633eb4a70c6c3cc957e370e057da1
Dwi Ana Ambar Rofiqoh <45792203+dwianaambarr@users.noreply.github.com>
2020-01-03 03:35:43 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,36 +1,41 @@
-package sample;
+//Insert package
+package sample; 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import sampel.Search;
 
 public class Main extends Application{
-    private TableView<Search> tableView = new TableView<Search>();
+    //Create table view
+    private TableView<Search> tableView = new TableView<sampel.Search>();
+    //Create data to show in table view
     private final ObservableList<Search> data
-	    = FXCollections.observableArrayList(
-	    new Search("Ambar", "Pensil", 10000),
-	    new Search("Ambar", "Pulpen", 15000),
-	    new Search("Ambar", "Buku", 20000),
-	    new Search("Ambar", "Kertas", 5000),
-	    new Search("Ekstensi", "Kursi", 200000),
-	    new Search("Ekstensi", "Meja", 500000),
-	    new Search("Ekstensi", "Lemari", 2000000),
-	    new Search("Ekstensi", "Kulkas", 5500000),
-	    new Search("Ekstensi", "AC", 5000000),
-	    new Search("Ekstensi", "Televisi", 4000000)
+            = FXCollections.observableArrayList(
+            new Search("Ambar", "Pensil", 10000),
+            new Search("Ambar", "Pulpen", 15000),
+            new Search("Ambar", "Buku", 20000),
+            new Search("Ambar", "Kertas", 5000),
+            new Search("Ekstensi", "Kursi", 200000),
+            new Search("Ekstensi", "Meja", 500000),
+            new Search("Ekstensi", "Lemari", 2000000),
+            new Search("Ekstensi", "Kulkas", 5500000),
+            new Search("Ekstensi", "AC", 5000000),
+            new Search("Ekstensi", "Televisi", 4000000)
     );
 
-    public static ArrayList<Store> storeList = new ArrayList<Store>();
-    @Override
+     @Override
     public void start(Stage primaryStage) throws Exception{
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10, 10, 10, 10));
@@ -73,13 +78,13 @@ public class Main extends Application{
         });
 
         TableColumn column1 = new TableColumn("Nama Store");
-        column1.setCellValueFactory(new PropertyValueFactory<Search, String>("namastore"));
+        column1.setCellValueFactory(new PropertyValueFactory<sampel.Search, String>("namastore"));
 
         TableColumn column2 = new TableColumn("Nama Barang");
-        column2.setCellValueFactory(new PropertyValueFactory<Search, String>("namabarang"));
+        column2.setCellValueFactory(new PropertyValueFactory<sampel.Search, String>("namabarang"));
 
         TableColumn column3 = new TableColumn("Harga");
-        column3.setCellValueFactory(new PropertyValueFactory<Search, String>("harga"));
+        column3.setCellValueFactory(new PropertyValueFactory<sampel.Search, String>("harga"));
 
         column0.setMinWidth(30);
         column1.setMinWidth(200);
@@ -93,7 +98,7 @@ public class Main extends Application{
 
         GridPane.setConstraints(tableView, 0, 1);
 
-        FilteredList<Search> filter = new FilteredList(data, p -> true);//Pass the data to a filtered list
+        FilteredList<sampel.Search> filter = new FilteredList(data, p -> true);//Pass the data to a filtered list
         tableView.setItems(filter);//Set the table's items using the filtered list
         tableView.getColumns().addAll(column0, column1, column2, column3);
 
@@ -111,10 +116,10 @@ public class Main extends Application{
 
 
     public static void main(String[] args) throws Exception {
-    	try{
-	        Application.launch(args);
-	    } catch(Exception ex){
-	        System.out.print(ex.getMessage() + "\n");
-	    }
-	}
+        try{
+            Application.launch(args);
+        } catch(Exception ex){
+            System.out.print(ex.getMessage() + "\n");
+        }
+    }
 }
